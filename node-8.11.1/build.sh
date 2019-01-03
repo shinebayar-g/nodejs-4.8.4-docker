0f0cbf7e51f5327e684e75e35d677128d2f7eae1
shinebayar-g <shinebayar.gansukh@gmail.com>
2019-01-03 16:35:24 +0800

    Include g++ in node-8.11.1

--- node-8.11.1/build.sh
+++ node-8.11.1/build.sh
@@ -1,5 +1,10 @@
 # Update upstream image before build
+echo "Updating upstream image debian:jessie-slim"
 docker pull debian:jessie-slim
+
 # Build & push
+echo "Building image erxes/node:8.11.1"
 docker build -t erxes/node:8.11.1 .
+
+echo "Pushing image erxes/node:8.11.1"
 docker push erxes/node:8.11.1
